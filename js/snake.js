f1dab28cf6b9d08e729af59705650c2b3dbaea24
philipauu <Phil@Phils-MacBook.local>
2017-12-19 16:30:29 +0800

    Speak softly and carry a +6 two-handed sword.

--- js/snake.js
+++ js/snake.js
@@ -1,29 +1,24 @@
-var snake;
-
-function setup() {
-    createCanvas(600, 600);
-    snake = new Snake();
-}
-
-function draw() {
-    background(51);
-    snake.update();
-    snake.show();
-}
-
 function Snake() {
     this.x = 0;
     this.y = 0;
     this.xspeed = 0;
     this.yspeed = 0;
 
+    this.dir = function (x, y) {
+        this.xspeed = x;
+        this.yspeed = y;
+    }
+
     this.update = function () {
-        this.x = this.x + this.xspeed;
-        this.y = this.y + this.yspeed;
+        this.x = this.x + this.xspeed*grid;
+        this.y = this.y + this.yspeed*grid;
+
+        this.x = constrain(this.x, 0, width-grid);
+        this.y = constrain(this.y, 0, width-grid);
     }
 
     this.show = function () {
         fill(255);
-        rect(this.x, this.y, 10, 10);
+        rect(this.x, this.y, grid, grid);
     }
 }
